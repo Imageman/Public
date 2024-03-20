045cd34eec48778232787fdf17fe15fa9c0b13a2
Vladimir Imageman72 <imageman72@gmail.com>
2024-03-20 10:44:01 +0200

    Automatic add_commit_push.bat

--- fill.js
+++ fill.js
@@ -2,6 +2,7 @@
 // КУЧА закладок от Владимира Чаплинского
 //LinkT("http","2024.01.01","keyword","Title","descr");
 //
+LinkT("https://messages.google.com/web/u/0/conversations","2024.03.20","google, services, сервисы","Сообщения СМС","Получение и отправление сообщений (SMS) с android телефона.");
 LinkT("https://www.bing.com/images/create/","2024.03.09","service, generator, AI","CopilotCopilot Designer","Сервис от Microsoft, DALL-E 3. Создание разнообразных картинок.");
 LinkT("https://github.com/xtekky/gpt4free","2024.03.03","GPT, AI, Python","gpt4free: Various collection of powerful language models","Халявный доступ к gpt-4, Llama, CodeLlama, Mixtral, gemini, claude из программы на питоне.");
 LinkT("https://keremerkan.net/qr-code-and-2d-code-generator/","2024.01.01","service, сервис, online, generator","QR Code and 2D Code Generator","Генератор QR-кодов (QRcode), Data Matrix, Aztec, Micro QR. Сокращение URL, выбор размера данных для восстановления, выбор типа данных.");
