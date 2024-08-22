af07295d33dc24fd621e25822e2ee2b3f785627d
Vladimir Imageman72 <imageman72@gmail.com>
2024-08-22 10:49:14 +0300

    Automatic add finance_plot.html

--- fill.js
+++ fill.js
@@ -1,7 +1,9 @@
 // Кириллический текст в виндовс кодровке UTF-8
 // КУЧА закладок от Владимира Чаплинского
-//LinkT("http","2024.06.01","keyword","Title","descr");
+//LinkT("http","2024.08.01","keyword","Title","descr");
 //
+LinkT("https://downdetector.com/","2024.08.21","online, test","Downdetector.com","Английский сервис для отслеживания работы рызличных сайтов и программ (telegram, skype, whatsapp, youtube, steam, microsoft, google, spotify, reddit, gmail, openai, tiktok)");
+LinkT("https://downdetector.su/","2024.08.21","online, test","Downdetector","Русский сервис для отслеживания работы рызличных сайтов и программ (telegram, skype, whatsapp, youtube, steam, кинопоиск, вконтакте)");
 LinkT("https://edit.paperpal.com/","2024.08.19","service, search, AI","Paperpal","Система для помощи в написании научных статей. Есть поиск по опубликованным научным статьям.");
 LinkT("https://morss.it/","2024.06.01","RSS, news-feed, service","Full-text RSS","Расширяет урезанные RSS ленты, очищает страницы(?).");
 LinkT("https://www.coze.com/home","2024.04.29","service, AI, GPT","Coze own AI bot","Множество AI ботов: GPT4, DALLE 3. Позволяет создать собстенного бота.");
